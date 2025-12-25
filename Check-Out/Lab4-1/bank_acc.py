1879d5a60f894ffb94e4d2231354324adcfba6b9
chatchana-Chaenban <chatchana.c@kkumail.com>
2025-12-25 13:08:30 +0700

    Update bank_acc.py

--- Check-Out/Lab4-1/bank_acc.py
+++ Check-Out/Lab4-1/bank_acc.py
@@ -6,47 +6,49 @@ P2
 
 class BankAccount:
 
-    saving_run = 0 # class attribute
-    loaning_run = 0
-    branch_number = 5511
-    branch_name = "KKU Complex"
+      saving_run = 0 # class attribute
+      loaning_run = 0
+      branch_number = 5511
+      branch_name = "KKU Complex"
 
-    def __init__(self, name, acc_type="saving", balance=0): # constructor
-        if not name:
+      def __init__(self, name, acc_type="saving", balance=0): # constructor
+         if not name:
            raise ValueError("Name is required") #if there are no name 
        
-        self.name = name # instance
-        self.type = acc_type
-        self.balance = balance
+         self.name = name # instance
+         self.type = acc_type
+         self.balance = balance
 
-        if self.type == "saving": # saving type
-           BankAccount.saving_run += 1
-           #type_code = 1
-           self.running = f"{BankAccount.branch_number}-1-{BankAccount.saving_run}"
+         if self.type == "saving": # saving type
+            BankAccount.saving_run += 1
+            #type_code = 1
+            self.acc_num = f"{BankAccount.branch_number}-1-{BankAccount.saving_run}"
         
-        elif self.type == "loan": # loaning type
-           BankAccount.loaning_run += 1
-           #type_code = 2
-           self.running = f"{BankAccount.branch_number}-2-{BankAccount.loaning_run}"
+         elif self.type == "loan": # loaning type
+            BankAccount.loaning_run += 1
+            #type_code = 2
+            self.acc_num = f"{BankAccount.branch_number}-2-{BankAccount.loaning_run}"
 
-        else:
-           raise ValueError("Invalid account type")
+         else:
+            raise ValueError("Invalid account type")
        
-    def get_balance(self):
-       return self.balance
+      def get_balance(self):
+         return self.balance
     
-    def deposit(self, amount=0): # deposit money
-      self.balance += amount
+      def deposit(self, amount=0): # deposit money
+         self.balance += amount
+         return self.balance
 
-    def withdraw(self, amount=0): # withdraw money
-      self.balance -= amount
+      def withdraw(self, amount=0): # withdraw money
+         self.balance -= amount
+         return self.balance
 
-    def print_customer(self):
-       print("----- Customer Record -----")
-       print(f"Name: {self.name}")
-       print(f"Account number: {self.running}")
-       print(f"Account type: {self.type}")
-       print(f"Balance: {self.balance}")
-       print("----- End Record -----")
+      def print_customer(self):
+         print("----- Customer Record -----")
+         print(f"Name: {self.name}")
+         print(f"Account number: {self.acc_num}")
+         print(f"Account type: {self.type}")
+         print(f"Balance: {self.balance}")
+         print("----- End Record -----")
 
     
